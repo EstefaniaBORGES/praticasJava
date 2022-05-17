fb729006799b80b9a633f075a034b2ebaa10970d
Estefânia Borges <estefaniadeoliveira@gmail.com>
2022-05-17 15:24:15 -0300

    exercicioJava02

---
+++ exe02.java
@@ -0,0 +1,21 @@
+
+
+import java.util.scanner;
+
+public class Exercicio02 {          
+    public static void main(String args) { 
+    Scanner input =new Scanner (System.in);
+        int x,y,z;
+    System.out.print("Informe o primeiro número:");
+    x=input.nextInt();
+    System.out.print("Informe o segundo número:");
+    y=input.nextInt();
+    System.out.print("Informe o terceiro número:");
+    z=input.nextInt();    
+   
+    System.out.println("O resultado é:"  );
+
+    }
+
+   
+}
