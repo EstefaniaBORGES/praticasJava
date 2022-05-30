f6c7e0edb8c3d6b5bca79b8cc31363085d4952e7
Estefânia Borges <estefaniadeoliveira@gmail.com>
2022-05-30 10:15:07 -0300

    exe03atv01

--- Exercicio03.java
+++ Exercicio03.java
@@ -1,8 +1,31 @@
 
 import java.util.Scanner;
 
-public class Exercicio03 {
-    public static void main
+public class Exercicio03 { 
+    public static void main (String [] args) {
+        Scanner input = new Scanner (System.in);
+       
+        int nota1,nota2,nota3,media; 
+       
+        
+            System.out.println("Informe a primeira nota:");
+            nota1 = input.nextInt();
+            System.out.println("informe a segunda nota:");
+             nota2 = input.nextInt();
+            System.out.println("informe a terceira nota:");
+            nota3 = input.nextInt();
+           
+            media = nota1 + nota2 + nota3;
+        System.out.println("A média das notas é:" + media);
+       
+       
+        
+        input.close();
+        }
+      
+    }
 
-    
-}
+
+
+      
+
