61188c6fa6e2dfd0952aa2a924987fd637948c5f
Estefânia Borges <71102994+EstefaniaBORGES@users.noreply.github.com>
2022-05-31 19:57:29 -0300

    Update Exe03Atv02.java

--- Exe03Atv02.java
+++ Exe03Atv02.java
@@ -1,19 +1,18 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Exe03Atv02 {    
- 
-    public static void main (String[]args) {
-        Scanner input = new Scanner (System.in);
-    int nascimento,atual,resultado;
-    int alvo = 2050;
-    
-    System.out.println("Informe o ano de nascimento");
-            nascimento = input.nextInt();
-            System.out.println("informe o ano atual");
-             atual = input.nextInt();
-             resultado = nascimento - atual;
-            System.out.println("Sua idade em 2050 será :" + resultado + alvo);        
-            
-            input.close();
+    public static void main(String args[]) {
+        int anoNascimento;
+        Scanner input = new Scanner(System.in);
+        LocalDate anoCorrente = LocalDate.now(); 
+        
+        System.out.println("Informe o ano de nascimento");
+        anoNascimento = input.nextInt();
+       
+        System.out.println("Idade: " + (anoCorrente.getYear() - anoNascimento));
+        System.out.println("Idade em 2050: " + (2050 - anoNascimento));
+
+        input.close();
     }
 }
