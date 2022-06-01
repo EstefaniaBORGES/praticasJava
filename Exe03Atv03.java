2b8edae6db9a416f2166cb8a8b8216aa0771fe38
Estefânia de Oliveira Malaquias Borges - 422142410 <422142410@ulife.com.br>
2022-05-31 21:16:54 -0300

    Exe03Atv03

---
+++ Exe03Atv03.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exe03Atv03 {
+    public static void main(String[] args) {
+        double cotacaoDolar, valorDolar, valorReal;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Conversor de Dolar em real");
+
+        System.out.println("Digite a cotação do dolar:");
+
+        cotacaoDolar = input.nextDouble();
+        System.out.println("Digite o valor em reais:");
+        valorReal = input.nextDouble();
+        
+
+    }
+
+}
